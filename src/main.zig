6b22bcefc809fdc1ce4bdfd1e4172aa0dedc5ae4
shybu8 <bushink04@proton.me>
2025-05-14 14:01:11 +0300

    WIP on new implementation

--- src/main.zig
+++ src/main.zig
@@ -14,31 +14,9 @@ pub fn GetVec2(T: type) type {
     };
 }
 
-// const WFlt = f32;
-// const Vec2 = GetVec2(WFlt);
-
 const page_alloc = std.heap.page_allocator;
-// const alloc = std.heap.allignedAllocator(page_alloc, 128);
-// const MAL = std.MultiArrayList;
-// const Soa = MAL(Vec2);
-const List = std.ArrayListAligned(f64, std.mem.Alignment.@"64");
 
 pub fn main() !void {
-    // my_simd.arraysOp(
-    //     f64,
-    //     a.items,
-    //     .add,
-    //     b.items,
-    //     b.items,
-    // );
-
-    // my_simd.arraysCopy(
-    //     f64,
-    //     b.items,
-    //     c.items,
-    // );
-    // std.mem.copyForwards(f64, c.items, a.items);
-    
     var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.posix.getrandom(std.mem.asBytes(&seed));
@@ -49,19 +27,23 @@ pub fn main() !void {
     var timer = std.time.Timer.start() catch unreachable;
 
     const T = f64;
+    const List = std.ArrayListAligned(T, std.mem.Alignment.@"64");
     const lengths = [_]usize {
-        128,
+        256,
         128 * 300,
         128 * 1024 * 2,
         128 * 1024 * 7,
         128 * 1024 * 128,
     };
     const scl_len = 2;
-    const pass_amount = 50;
+    const pass_amount = 100;
+    dprint("\tPerformance test of\n\t\tb[] = a[] + [(A, B), (A, B), ...]\n\twith {} as individual element\n\t(simd_heler vs llvm vectoried loop)\n\n", .{T});
+
     dprint("Pass amount: {}\n\n", .{pass_amount});
 
     for (lengths) |arr_len| {
-        dprint("Array length: {}\n", .{arr_len});
+        const mib_size = @as(f64, @floatFromInt(@bitSizeOf(T) / 8 * arr_len)) / 1024 / 1024;
+        dprint("Array length: {}\tMiB size: {d:.3}\n", .{arr_len, mib_size});
         var llvm_avg: usize = 0;
         var my_avg: usize = 0;
         var correctness: bool = true;
@@ -76,7 +58,7 @@ pub fn main() !void {
             b2.deinit();
             s.deinit();
         }
-        var r_val: f64 = 0;
+        var r_val: T = 0;
 
         for (0..arr_len) |_| {
             r_val = rand.float(T);
@@ -93,15 +75,15 @@ pub fn main() !void {
             if (rand.boolean()) {
                 timer.reset();
                 my_simd.arrayMultielemScalarOp(
-                    2, f64,
-                    a.items, .add, s.items[0..2].*,
-                    b2.items,
+                    scl_len, T,
+                    a.items, .add, s.items[0..scl_len].*,
+                    b2.items, .non_temporal,
                 );
                 my_avg += timer.read();
 
                 timer.reset();
                 var i: usize = 0;
-                while (i < a.items.len) : (i += scl_len) {
+                while (i + scl_len <= a.items.len) : (i += scl_len) {
                     inline for (0..scl_len) |j| {
                         b1.items[i + j] = a.items[i + j] + s.items[j];
                     }
@@ -110,7 +92,7 @@ pub fn main() !void {
             } else {
                 timer.reset();
                 var i: usize = 0;
-                while (i < a.items.len) : (i += scl_len) {
+                while (i + scl_len <= a.items.len) : (i += scl_len) {
                     inline for (0..scl_len) |j| {
                         b1.items[i + j] = a.items[i + j] + s.items[j];
                     }
@@ -119,9 +101,9 @@ pub fn main() !void {
 
                 timer.reset();
                 my_simd.arrayMultielemScalarOp(
-                    2, f64,
-                    a.items, .add, s.items[0..2].*,
-                    b2.items,
+                    scl_len, T,
+                    a.items, .add, s.items[0..scl_len].*,
+                    b2.items, .non_temporal
                 );
                 my_avg += timer.read();
             }
@@ -133,29 +115,8 @@ pub fn main() !void {
         my_avg /= pass_amount;
         const speedup = 1 / (@as(f64, @floatFromInt(my_avg)) / @as(f64, @floatFromInt(llvm_avg)));
 
-        dprint("llvm_avg: {}\n  my_avg: {}\ncorrectness: {}\nspeedup: {d}\n\n",
+        dprint("llvm_avg: {} ns\nhlpr_avg: {} ns\ncorrectness: {}\nspeedup: {d}\n\n",
             .{llvm_avg, my_avg, correctness, speedup});
     }
-
-
-    // if (false) {
-    //     dprint("\n      ", .{});
-    //     for (a.items) |v| {
-    //         dprint("{d:.1} ", .{v});
-    //     }
-    //     dprint("\n      ", .{});
-    //     for (s.items) |v| {
-    //         dprint("{d:.1} ", .{v});
-    //     }
-    //     dprint("\nllvm: ", .{});
-    //     for (b1.items) |v| {
-    //         dprint("{d:.1} ", .{v});
-    //     }
-    //     dprint("\n  my: ", .{});
-    //     for (b2.items) |v| {
-    //         dprint("{d:.1} ", .{v});
-    //     }
-    //     dprint("\n", .{});
-    // }
 }
 
--- src/simd.zig
+++ src/simd.zig
@@ -11,6 +11,11 @@ pub const VecOperation = enum {
     div,
 };
 
+pub const NonTemporalOption = enum {
+    non_temporal,
+    usual,
+};
+
 inline fn vecOp(
     comptime N: usize,
     comptime T: type,
@@ -18,13 +23,47 @@ inline fn vecOp(
     comptime op: VecOperation,
     rhs: @Vector(N, T),
     dest: *align(1) @Vector(N, T),
+    comptime nt: NonTemporalOption,
 ) void {
-    switch (op) {
-        .add => dest.* = lhs + rhs,
-        .sub => dest.* = lhs - rhs,
-        .mul => dest.* = lhs * rhs,
-        .div => dest.* = lhs / rhs,
+    const res = switch (op) {
+        .add => lhs + rhs,
+        .sub => lhs - rhs,
+        .mul => lhs * rhs,
+        .div => lhs / rhs,
+    };
+    if (nt == .non_temporal and builtin.cpu.arch == .x86_64) {
+        switch (@typeInfo(T)) {
+            .float => switch (@sizeOf(T)) {
+                4 => asm volatile (
+                    \\ vmovntps %[vec], (%[ptr])
+                    :
+                    : [ptr] "r"(dest), [vec] "v"(res)
+                    : "memory"
+                ),
+                8 => asm volatile (
+                    \\ vmovntpd %[vec], (%[ptr])
+                    :
+                    : [ptr] "r"(dest), [vec] "v"(res)
+                    : "memory"
+                ),
+                else => @compileError("unsupported float width"),
+            },
+            .int => asm volatile (
+                \\ vmovntdq %[vec], (%[ptr])
+                :
+                : [ptr] "r"(dest), [vec] "v"(res)
+                : "memory"
+            ),
+            else => @compileError("unsupported element type for vecOp"),
+        }
+    } else {
+        dest.* = res;
     }
+}
+
+inline fn vecOpFlush() void {
+    if (builtin.cpu.arch == .x86_64)
+        asm volatile ("sfence" ::: "memory");
 }
 
 inline fn tailOp(
@@ -51,10 +90,11 @@ pub fn arraysOp(
     dest: []T
 ) void {
     assert(lhs.len == rhs.len and lhs.len == dest.len);
+    @compileLog(simd.suggestVectorLength(T));
     const N = simd.suggestVectorLength(T) orelse 1;
 
     var i: usize = 0;
-    
+
     while (i + N <= lhs.len) : (i += N) {
         const a = @as(
             *align(1) const @Vector(N, T), 
@@ -68,12 +108,13 @@ pub fn arraysOp(
             *align(1) @Vector(N, T),
             @ptrCast(dest[i..i + N])
         );
-        vecOp(N, T, a.*, op, b.*, d);
+        vecOp(N, T, a.*, op, b.*, d, .non_temporal);
     }
 
     while (i < lhs.len) : (i += 1) {
         tailOp(T, lhs, op, rhs, dest, i);
     }
+    vecOpFlush();
 }
 
 pub fn arraysCopy(
@@ -109,6 +150,7 @@ pub fn arrayMultielemScalarOp(
     comptime op: VecOperation,
     scalar: [L]T,
     dst: []T,
+    comptime nt: NonTemporalOption,
 ) void {
     assert(
         arr.len == dst.len and
@@ -116,7 +158,7 @@ pub fn arrayMultielemScalarOp(
     );
     const N = simd.suggestVectorLength(T) orelse 1;
     const _window_mul = N / L;
-    // TODO: Asynmetric vectors on L > N
+    // TODO: Asynmetric/wide window vectors on L > N
     const window_mul = if (_window_mul == 0) 1 else _window_mul;
     const window = window_mul * L;
 
@@ -126,13 +168,13 @@ pub fn arrayMultielemScalarOp(
     while (i + window <= arr.len) : (i += window) {
         const lhs = @as(
             *align(1) const @Vector(window, T),
-            @ptrCast(arr[i..i + window])
+            @ptrCast(&arr[i])
         );
         const d = @as(
             *align(1) @Vector(window, T),
-            @ptrCast(dst[i..i + window])
+            @ptrCast(&dst[i])
         );
-        vecOp(window, T, lhs.*, op, rhs, d);
+        vecOp(window, T, lhs.*, op, rhs, d, nt);
     }
     if (window_mul != 1) {
         // Tail
@@ -140,20 +182,181 @@ pub fn arrayMultielemScalarOp(
         while (i < arr.len) : (i += L) {
             const t_lhs = @as(
                 *align(1) const @Vector(L, T),
-                @ptrCast(arr[i..i + L])
+                @ptrCast(&arr[i])
             );
             const t_d = @as(
                 *align(1) @Vector(L, T),
-                @ptrCast(dst[i..i + L])
+                @ptrCast(&dst[i])
             );
-            vecOp(L, T, t_lhs.*, op, t_rhs, t_d);
+            vecOp(L, T, t_lhs.*, op, t_rhs, t_d, .usual);
+        }
+    }
+    if (nt == .non_temporal)
+        vecOpFlush();
+}
+
+pub const Operation = enum {
+    add,
+    sub,
+    mul,
+    div,
+};
+
+fn doVecOp(
+    comptime len: usize,
+    comptime T: type,
+    lhs: @Vector(len, T),
+    comptime op: Operation,
+    rhs: @Vector(len, T)
+) @Vector(len, T) {
+    return switch (op) {
+        .add => lhs + rhs,
+        .sub => lhs - rhs,
+        .mul => lhs * rhs,
+        .div => lhs / rhs,
+    };
+}
+
+pub const NonTemporalStoreOption = enum {
+    usual_store,
+    nt_store,
+};
+
+inline fn nt_flush() void {
+    if (builtin.cpu.arch == .x86_64)
+        asm volatile ("sfence" ::: "memory");
+}
+
+fn lcm(a: comptime_int, b: comptime_int) comptime_int {
+    var m = @max(a, b);
+    while (m % a != 0 or m % b != 0) {
+        m += @max(a, b);
+    }
+    return m;
+}
+
+pub fn arrayWideScalarOp(
+    comptime scalar_len: usize,
+    comptime T: type,
+    arr: []const T,
+    comptime op: Operation,
+    scalar: [scalar_len]T,
+    dst: []T,
+    comptime nt: NonTemporalStoreOption,
+) void {
+    const simd_len = simd.suggestVectorLength(T) orelse 1;
+    const scl_window_len = lcm(simd_len, scalar_len);
+    const scl_window = @as(
+        [scl_window_len]T,
+        @bitCast(simd.repeat(
+            scl_window_len,
+            scalar
+        ))
+    );
+    var i: usize = 0;
+    while (i + scl_window_len <= arr.len) : (i += scl_window_len) {
+        const arr_window = @as(
+            *align(1) const @Vector(scl_window_len, T),
+            @ptrCast(&arr[i])
+        );
+        const dst_window = @as(
+            *align(1) @Vector(scl_window_len, T),
+            @ptrCast(&dst[i])
+        );
+        wideWindowsOp(
+            scl_window_len, T,
+            arr_window.*, op, scl_window,
+            dst_window, nt
+        );
+    }
+    while (i + simd_len <= arr.len) : (i += simd_len) {
+        const arr_tail = @as(
+            *align(1) const @Vector(simd_len, T),
+            @ptrCast(&arr[i])
+        );
+        const scl_ind = i % scl_window_len;
+        const scl_tail = @as(
+            *align(1) const @Vector(simd_len, T),
+            @ptrCast(&scl_window[scl_ind])
+        );
+        const dst_tail = @as(
+            *align(1) @Vector(simd_len, T),
+            @ptrCast(&dst[i])
+        );
+        // Without nt
+        dst_tail.* = arr_tail.* + scl_tail.*;
+    }
+    if (nt == .nt_store)
+        nt_flush();
+}
+
+inline fn divideVec(
+    comptime len: usize,
+    comptime T: type,
+    src: *align(1) @Vector(len, T),
+    comptime sublen: usize
+) [len / sublen]*align(1) @Vector(sublen, T) {
+    const as_ptr = @as(*align(1) [len]T, @ptrCast(src));
+    var res: [len / sublen]*align(1) @Vector(sublen, T) = undefined;
+    for (0..len / sublen) |i| {
+        const start = sublen * i;
+        res[i] = @as(
+            *align(1) @Vector(sublen, T),
+            @ptrCast(&as_ptr[start])
+        );
+    }
+    return res;
+}
+
+inline fn wideWindowsOp(
+    comptime window_len: usize,
+    comptime T: type,
+    lhs: @Vector(window_len, T),
+    comptime op: Operation,
+    rhs: @Vector(window_len, T),
+    dst: *align(1) @Vector(window_len, T),
+    comptime nt: NonTemporalStoreOption,
+) void {
+    // window_len is guaranteed multiple of simd_len
+    const simd_len = simd.suggestVectorLength(T) orelse 1;
+    const lhs_portions = divideVec(
+        window_len, T,
+        @constCast(&lhs), simd_len
+    );
+    const rhs_portions = divideVec(
+        window_len, T,
+        @constCast(&rhs), simd_len
+    );
+    const dst_portions = divideVec(
+        window_len, T,
+        dst, simd_len
+    );
+    inline for (0..window_len / simd_len) |i| {
+        const res = doVecOp(
+            simd_len, T,
+            lhs_portions[i].*,
+            op,
+            rhs_portions[i].*,
+        );
+        if (nt == .usual_store or builtin.cpu.arch != .x86_64) {
+            dst_portions[i].* = res;
+        } else switch (@typeInfo(T)) {
+            .float => switch(@sizeOf(T)) {
+                4 => asm volatile (
+                    \\ vmovntps %[vec], (%[ptr])
+                    :
+                    : [ptr] "r"(dst_portions[i]), [vec] "v"(res)
+                    : "memory"
+                ),
+                8 => asm volatile (
+                    \\ vmovntpd %[vec], (%[ptr])
+                    :
+                    : [ptr] "r"(dst_portions[i]), [vec] "v"(res)
+                    : "memory"
+                ),
+                else => @compileError("Not implemented size"),
+            },
+            else => @compileError("Not implemented"),
         }
     }
 }
-
-// pub fn arraysMultielemTimelocalOps(
-//     comptime L: usize,
-//     comntime T: type,
-//     comptime C: usize,
-//     srcs: [C][]T,
-//     comptime func: fn ([C]@Vector(
